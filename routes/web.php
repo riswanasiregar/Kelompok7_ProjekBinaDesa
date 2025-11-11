a10ebf1d023b8d55ade9d0c9abdd09fcc07f4400
riswanasiregar <riswana24si@mahasiswa.pcr.ac.id>
2025-11-11 09:04:57 +0700

    template admin riswana

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+//dashboard
 Route::get('/', function () {
     return view('dashboard');
 });
+//layout without menu
+Route::get('/layout-without-menu', function () {
+    return view('layout-without-menu');
+})->name('layout.without.menu');
+//login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+//layout without navbar
+Route::get('/layout-without-navbar', function () {
+    return view('layout-without-navbar');
+})->name('layout.without.navbar');
+
+
